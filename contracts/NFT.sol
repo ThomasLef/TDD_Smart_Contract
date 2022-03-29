58e44b8007b58e7824616b5640c26d753686b774
Thomas Lefebvre <thomaslefebvre1999@orange.fr>
2022-03-29 12:16:42 +0200

    Deleting simpleStorage and creation of transfer function

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: CentraleSupelec
 pragma solidity >=0.4.22 <0.9.0;
 
 
@@ -13,7 +14,18 @@ contract NFT {
         owner = msg.sender;
     }
 
+    function transfer(address _to) public {
+        require(owner == msg.sender, "You are not the owner.");
+        owner = _to;
+    }
+
+    // Getters
+
     function getName() public view returns (string memory) {
         return name;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
     }
 }
--- test/NFT.test.js
+++ test/NFT.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { globalAgent } = require('http');
 const NFT = artifacts.require('NFT');
 
 contract('NFT', async (account) => {
@@ -7,5 +8,12 @@ contract('NFT', async (account) => {
     const nft = await NFT.deployed();
     await nft.mint("test","This is a test nft");
     assert(await nft.getName() == "test")
+  });
+
+  it('Should be able to transfer the NFT', async () => {
+    const nft = await NFT.deployed();
+    assert(await nft.getOwner() == "0x3eF6d061E91F3CD7aC5ccb2AD689c63B596Dc7D1")
+    await nft.transfer("0x56Cd066E2A0215E40e89Da4e444358C6fB55AbA3")
+    assert(await nft.getOwner() == "0x56Cd066E2A0215E40e89Da4e444358C6fB55AbA3")
   });
 });
