4aba1f18ca16bccd6b3e465bca15a03d472fdbd4
Thomas Lefebvre <thomaslefebvre1999@orange.fr>
2022-04-01 11:38:01 +0200

    Nouveaux tests pour nouvelle vie

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,16 +7,26 @@ contract NFT {
     address public owner;
     string public name;
     string public description;
+    uint public price = 0;
+    bool public isForSale = false;
 
     function setInfo(string calldata _name, string calldata _description) public {
         name = _name;
         description = _description;
-        owner = msg.sender;
+        owner = tx.origin;
     }
 
     function transfer(address _to) public {
-        require(owner == msg.sender, "You are not the owner.");
+        require(owner == tx.origin, "You are not the owner.");
         owner = _to;
+    }
+
+    function setPrice(uint _price) public {
+        price = _price;
+    }
+
+    function putUpForSale() public {
+        isForSale = true;
     }
 
     // Getters
@@ -27,6 +37,14 @@ contract NFT {
 
     function getOwner() public view returns (address) {
         return owner;
+    }
+
+    function getPrice() public view returns (uint) {
+        return price;
+    }
+
+    function isItForSale() public view returns (bool) {
+        return isForSale;
     }
 }
 
@@ -39,12 +57,24 @@ contract NFTlist {
         return test[0];
     }
 
+    // Main functions
+
     function mint(string calldata _name, string calldata _description) public {
         nfts.push(new NFT());
         nfts[nfts.length - 1].setInfo(_name, _description);
     }
 
+    function transfer(uint id, address _to) public {
+        nfts[id].transfer(_to);
+    }
+
+    // Getters
+
     function getNFTName(uint id) public view returns (string memory) {
         return nfts[id].getName();
+    }
+
+    function getOwner(uint id) public view returns (address) {
+        return nfts[id].getOwner();
     }
 }
--- test/NFT.test.js
+++ test/NFT.test.js
@@ -36,9 +36,33 @@ contract('NFTlist', async (account) => {
 
   it('Should be able to mint two separated NFTs', async () => {
     const nftlist = await NFTlist.deployed();
-    await nftlist.mint("test1", "This is a first NFT");
+    await nftlist.mint("test1", "This is a first NFT", {from: account[0]});
     await nftlist.mint("test2", "This is a second NFT");
     assert(await nftlist.getNFTName(0) == "test1");
     assert(await nftlist.getNFTName(1) == "test2");
+  });
+
+  it('Should be able to transfer when there are several NFTs', async () => {
+    const nftlist = await NFTlist.deployed();
+    assert(await nftlist.getOwner(0) == account[0])
+    await nftlist.transfer(0, account[1])
+    assert(await nftlist.getOwner(0) == account[1])
+  });
+
+});
+
+contract('NFTsales', async () => {
+
+  it('Should be able to set a price of an NFT', async () => {
+    const nft = await NFT.deployed();
+    await nft.setInfo("test1", "This is a test NFT");
+    await nft.setPrice(30);
+    assert(await nft.getPrice() == 30);
+  });
+
+  it('Should be able to put up for sale an NFT', async () => {
+    const nft = await NFT.deployed();
+    await nft.putUpForSale();
+    assert(await nft.isItForSale() == true);
   });
 });
